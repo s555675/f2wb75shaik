9172721e10be3080c1c86ce919e99f98497d653a
s555675 <111791495+s555675@users.noreply.github.com>
2022-11-01 16:57:40 -0500

    changes made in pizza.js

--- routes/pizza.js
+++ routes/pizza.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pizza ', { title: 'Express' });
+  res.render('pizza ', { title: 'search result pizza ' });
 });
 
 module.exports = router;
