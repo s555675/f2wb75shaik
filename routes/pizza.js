b09ca2863e0cfe7462e47d054c55e312a1349c6e
s555675 <111791495+s555675@users.noreply.github.com>
2022-11-01 17:07:59 -0500

    removed unnecessary files from git

--- routes/pizza.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('pizza ', { title: 'search result pizza ' });
-});
-
-module.exports = router;
