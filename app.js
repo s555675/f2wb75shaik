329eb3dbfe3128af9820aa1a3f0d15af6787050c
s555675 <111791495+s555675@users.noreply.github.com>
2022-11-01 17:11:15 -0500

    updated the class files to git

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var pizzaRouter = require('./routes/pizza');
 
+
 var app = express();
 
 // view engine setup
@@ -22,7 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/pizza ', pizzaRouter);
+app.use('/pizza', pizzaRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
