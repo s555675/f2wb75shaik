ce9489e3b68f3ef0b8294007dde8a128cbca6f09
s555675 <111791495+s555675@users.noreply.github.com>
2022-11-02 17:39:41 -0500

    updated app.js file

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var yogurtRouter = require('./routes/yogurt');
 var gridbuildRouter = require('./routes/gridbuild');
+var selectorRouter = require('./routes/selector');
 
 
 var app = express();
@@ -26,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/yogurt', yogurtRouter);
 app.use('/gridbuild', gridbuildRouter);
+app.use('/selector', selectorRouter);
 
 
 // catch 404 and forward to error handler
