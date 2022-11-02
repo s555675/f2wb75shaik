a1b824c5bdaae70be6f61077286a669c4cea26a5
s555675 <111791495+s555675@users.noreply.github.com>
2022-11-02 17:11:19 -0500

    updated the yogurt file.js

--- routes/yogurt.js
+++ routes/yogurt.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('yogurt', { title: 'Seacrh results yogurt' });
+  res.render('yogurt', { title: 'search results yogurt' });
 });
 
 
